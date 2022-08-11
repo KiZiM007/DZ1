52ba2480ba788f836972de0ef07969361c142dfc
Kim <kim_konstantinov@bk.ru>
2022-08-11 14:42:16 +0300

    Save Number13/gitignore

---
+++ Number13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int Metod(int num)
+{
+    if (num > 99 || num < -99)
+    {
+        while (num > 999 || num < -999)
+        {
+            num = num / 10;
+        }
+        return num % 10;
+    }
+    else return -1;
+}
+int result = Metod(number);
+if (result == -1) Console.Write($"В числе {number} нет третьей цифры");
+else Console.Write($"{result}");
