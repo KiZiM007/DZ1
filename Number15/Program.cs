f07080da848607eb4cded5578b67028e6b27b9ba
Kim <kim_konstantinov@bk.ru>
2022-08-11 14:44:26 +0300

    Save Number15

---
+++ Number15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+int day(int num)
+{
+    if (num <= 7 && num >= 1)
+    {
+        return num > 5 ? 1 : 2;
+    }
+    else return -1;
+}
+int result = day(number);
+string res;
+if (result > 0)
+{
+  res = result == 1 ? "Выходные": "Будни";
+  Console.Write($"{number} -> {res}");
+}
+else Console.Write("Нет такого дня недели");
